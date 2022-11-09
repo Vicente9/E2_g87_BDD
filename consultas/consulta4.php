c746e9e298cff8cc103bf17ff6f612ae03d1cdbe
Vicente9 <101224081+Vicente9@users.noreply.github.com>
2022-11-09 15:52:27 -0300

    Add files via upload

---
+++ consultas/consulta4.php
@@ -0,0 +1,29 @@
+<?php include('../templates/header.html');   ?>
+
+<body>
+
+<?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion.php");
+
+	$Dataso = $_POST["dato"];
+	
+
+ 	$query = "SELECT DISTINCT Eventos.pais FROM Eventos, Tour Where Tour.nombre_tour = eventos.evento AND nombre_tour = $Dataso;";
+	$result = $db -> prepare($query);
+	$result -> execute();
+	$paises = $result -> fetchAll();
+  ?>
+
+	<table>
+    <tr>
+      <th>Artista</th>
+    </tr>
+  <?php
+	foreach ($paises as $paises) {
+  		echo "<tr> <td>$paises[0]</td>";
+	}
+  ?>
+	</table>
+
+<?php include('../templates/footer.html'); ?>
