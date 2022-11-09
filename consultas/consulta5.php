4c2dec2e12b0145c1d5ad5996f7a0cd5700b9633
Lucasundu <101349286+Lucasundu@users.noreply.github.com>
2022-11-09 15:57:51 -0300

    Create consulta5.php

---
+++ consultas/consulta5.php
@@ -0,0 +1,29 @@
+<?php include('../templates/header.html');   ?>
+
+<body>
+
+<?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion.php");
+
+	$nombre = $_POST["nombre_artista"];
+	
+
+ 	$query = "SELECT nombre_productora FROM Eventos Where nombre_artistico LIKE '%$nombre%';";
+	$result = $db -> prepare($query);
+	$result -> execute();
+	$productoras = $result -> fetchAll();
+  ?>
+
+	<table>
+    <tr>
+      <th>Nombre Productora</th>
+    </tr>
+  <?php
+	foreach ($productoras as $productora) {
+  		echo "<tr> <td>$productora[0]</td> </tr>";
+	}
+  ?>
+	</table>
+
+<?php include('../templates/footer.html'); ?>
