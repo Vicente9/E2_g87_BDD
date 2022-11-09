64a80af0ca0f8c91b41cc87e6228330e30968fcb
Vicente9 <101224081+Vicente9@users.noreply.github.com>
2022-11-09 15:01:20 -0300

    Update consulta2.php

--- consultas/consulta2.php
+++ consultas/consulta2.php
@@ -7,7 +7,7 @@
   require("../config/conexion.php");
 
   #Se obtiene el valor del input del usuario
-  $nombre = $_POST["nombre_artistico"];
+  $nombre = $_POST["nombre_artistico_elegido"];
 
   #Se construye la consulta como un string
  	$query = "SELECT count(nombre_artistico) AS veces FROM Entradas_cortesia WHERE nombre_artistico = $nombre;";
