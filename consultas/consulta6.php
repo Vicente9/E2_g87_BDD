1f8b0893ed19602c111a5bf0af45040472f22f61
Vicente9 <101224081+Vicente9@users.noreply.github.com>
2022-11-09 16:29:22 -0300

    Add files via upload

---
+++ consultas/consulta6.php
@@ -0,0 +1,46 @@
+
+
+<?php include('../templates/header.html');   ?>
+
+<body>
+<?php
+  
+  require("../config/conexion.php");
+
+  
+  $nombre = $_POST["nombre_artista"];
+
+  
+     $query = "SELECT hotel, COUNT(hotel) AS veces FROM Hospedaje_y_Traslado Where LOWER(nombre_artistico)=LOWER('$nombre') GROUP BY hotel;";
+
+  
+    $result = $db -> prepare($query);
+    $result -> execute();
+    $respuesta = $result -> fetchAll();
+  ?>
+
+  <table>
+    <tr>
+      <th> veces </th>
+    </tr>
+
+      <?php
+        
+        foreach ($respuesta as $respuesta) {
+            echo "<tr> <td>$respuesta[0]</td> </tr>";
+        }
+      ?>
+
+  </table>
+
+
+
+
+
+
+
+
+
+
+
+
